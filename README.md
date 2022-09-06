294c4bf19302bad7bfbf7035237ca8e5abdc75a0
Samuel Heck <svheck2016@gmail.com>
2022-09-06 10:47:32 -0300

    primeiro commit

---
+++ README.md
@@ -0,0 +1,7 @@
+ ## CRUD Simples ReactJS
+
+Cadastro de Clientes simples feito em ReactJS. 
+
+Projeto feito juntamente com um tutorial de Youtube, com intuito de aprimorar o ReactJS.
+
+Video de referência: [https://youtu.be/FqbgZ3lwGnM]
---
+++ src/App.jsx
@@ -0,0 +1,94 @@
+import { EditIcon, DeleteIcon } from "@chakra-ui/icons"
+import {
+  Box,
+  Flex,
+  Button,
+  useDisclosure,
+  Table,
+  Thead,
+  Tr,
+  Th,
+  Tbody,
+  Td,
+  useBreakpointValue
+} from "@chakra-ui/react"
+import { useState, useEffect } from "react"
+import ModalComp from "./components/ModalComp"
+
+const App = () => {
+  const { isOpen, onOpen, onClose} = useDisclosure()
+  const [data, setData] = useState([])
+  const [dataEdit, setDataEdit] = useState({})
+
+  const isMobile = useBreakpointValue({
+    base: true,
+    lg: false,
+  })
+
+  useEffect(() => {
+    const db_costumer = localStorage.getItem('cad_cliente')
+      ? JSON.parse(localStorage.getItem("cad_cliente"))
+      : [];
+    setDataEdit(db_costumer)   
+  }, [setData]);
+
+  const handleRemove = (email) => {
+    const newArray = data.filter((item) => item.email !== email);
+
+    setData(newArray)
+
+    localStorage.setItem("cad_cliente", JSON.stringify(newArray))
+   }
+  
+  return (
+    <Flex h="100vh" align="center" justify="center" font-size="20px" fontFamily="poppins" bg="#212121" color="#ffffff">
+      <Box maxWidth={800} w="100%" h="100vh" py={10} px={2}>
+        <Button colorScheme="blue" onClick={() => [setDataEdit({}), onOpen()]}>
+          NOVO CADASTRO
+        </Button>
+        <Box overflowY="auto" height="100%">
+          <Table mt={6}>
+            <Thead>
+              <Tr>
+                <Th maxW={isMobile ? 5 : 100} fontSize="20px" color="ffffff">
+                  Nome
+                </Th>
+                <Th maxW={isMobile ? 5 : 100} fontSize="20px" color="ffffff">
+                  Email
+                </Th>
+                <Th p={0}></Th>
+                <Th p={0}></Th>
+              </Tr>
+            </Thead>
+            <Tbody>
+              {data.map(({name, email}, index) => (
+                <Tr key={index} _hover={{bg: "#343434"}}>
+                  <Td maxW={isMobile ? 5 : 100}>{name}</Td>
+                  <Td maxW={isMobile ? 5 : 100}>{email}</Td>
+                  <Td p={0}>
+                    <EditIcon fontSize={20} cursor="pointer" onClick={() => [setDataEdit({name, email, index}), onOpen()]}/>
+                  </Td>
+                  <Td p={0}>
+                    <DeleteIcon fontSize={20} cursor="pointer" onClick={() => handleRemove(email)}/>
+                  </Td>
+                </Tr>
+              ))}
+            </Tbody>
+          </Table>
+        </Box>
+      </Box>
+      {isOpen && (
+        <ModalComp 
+        isOpen={isOpen}
+        onClose={onClose}
+        data={data}
+        setData={setData}
+        dataEdit={dataEdit}
+        setDataEdit={setDataEdit}
+        />
+      )}
+    </Flex>
+  )
+}
+
+export default App
